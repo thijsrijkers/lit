64cc7fbc78a1a1d144e5a91fda294bce66996d9b
thijsrijkers <thijsrijkers@thijsrijkerss-MacBook-Pro.local>
2025-04-17 12:55:37 +0200

    changed main and added lit.yml example

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-
 	"lit/container"
 )
 
 func main() {
-	configPath := "lit.yml"
-	if len(os.Args) > 1 {
-		configPath = os.Args[1]
+	runner := container.Container{
+		ConfigPath: "lit.yml",
+		MountBase:  ".", // Or absolute path to your rootfs layout
 	}
-
-	config, err := container.LoadConfig(configPath)
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	fmt.Printf("Running container from image: %s\n", config.Image)
-	if err := container.RunContainer(config); err != nil {
-		log.Fatalf("Container failed: %v", err)
+	if err := runner.Run(); err != nil {
+		log.Fatalf("Container run failed: %v", err)
 	}
 }
+
